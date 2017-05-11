9f5915e4fb3a77eb427bc5b6e86c87b77fa88d16
Hannah King <hmking.nc@gmail.com>
2017-05-11 18:09:09 -0400

    fixed isverified cookie so that new registers cannot log in

--- routes/users.js
+++ routes/users.js
@@ -89,7 +89,7 @@ router.post('/login',
 	function(req, res, next) {
 		passport.authenticate('local', function(err, user, info) {
 			if (err) { return next(err); }
-			if (!user) { return res.redirect('/'); }
+			if (!user || user.isverified !== true) { return res.redirect('/'); }
 			req.logIn(user, function(err) {
 				if (err) { return next(err); }
 				req.session.isAuth = true;
