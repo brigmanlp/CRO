ec2e04f41e800cef31bd93b5c9bc7adb01cb6019
Hannah King <hmking.nc@gmail.com>
2017-05-16 23:14:03 -0400

    db schema set up for video storage

---
+++ models/video.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+// Video Schema
+var VideoSchema = mongoose.Schema({
+	name: {
+		type: String
+	},
+	category: {
+		type: String
+	},
+    url: {
+		type: String,
+	},
+	dateAdded: {
+		type: Date,
+        Default: Date.now()
+	}
+});
+
+var Video = module.exports = mongoose.model('Video', VideoSchema);
+
+module.exports.createVideo = function(newVideo, callback){
+	        newVideo.save(callback);
+}
+module.exports.getVideos = function(callback){
+	var query = {};
+	Video.find(query, callback);
+}
+// module.exports.getVideoByVideoname = function(Videoname, callback){
+// 	var query = {Videoname: Videoname};
+// 	Video.findOne(query, callback);
+// }
+
+// module.exports.getVideoById = function(id, callback){
+// 	Video.findById(id, callback);
+// }
+
+// module.exports.updateVideoById = function(id){
+// 	var query = {_id: id}
+// 	Video.update(query, { $set: {key: value}}).exec(
+// 		console.log("updated record")
+// 	);
+// }
