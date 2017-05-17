a2f1e18cce8a5fd6ab296f8a2470835a3316d263
Hannah King <hmking.nc@gmail.com>
2017-05-16 23:04:58 -0400

    modified misspelled variable

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var app = express();
 var routes = require('./routes/auth');
 var users = require('./routes/users');
 var verify = require('./routes/verify');
-var verify = require('./routes/videos');
+var videos = require('./routes/videos');
 
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
